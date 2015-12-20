cc703db80419521a627af20024a0e294b62461df
emcako <emcako@abv.bg>
2015-12-20 03:29:32 +0200

    Adding camera for taking an account picture in AccountPage

--- EatFast/EatFast/MainPage.xaml.cs
+++ EatFast/EatFast/MainPage.xaml.cs
@@ -54,8 +54,7 @@ namespace Teamer
             set { this.DataContext = value; }
         }
 
-        
-
+       
         //public void NavigateToPage(object sender, RoutedEventArgs args)
         //{
         //    this.Frame.Navigate(typeof(LocationsPage));
